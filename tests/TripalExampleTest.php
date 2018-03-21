6c6eeb62281007fc019aa04ac668709744f3f689
Bradford Condon <bradford.condon@gmail.com>
2018-03-21 16:29:49 -0400

    add travis

---
+++ tests/TripalExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+use StatonLab\TripalTestSuite\TripalTestCase;
+
+class TripalTestCase extends TripalTestCase {
+
+public function testExampleTest() {
+  $this->assertTrue(true);
+  }
+}
